53f047e09f5eec99739f162e1b6e1acc14071046
oznu <dev@oz.nu>
2019-05-07 15:53:21 +1000

    removed material deps

--- projects/angular6-json-schema-form/src/lib/framework-library/index.ts
+++ projects/angular6-json-schema-form/src/lib/framework-library/index.ts
@@ -3,8 +3,8 @@ export { FrameworkLibraryService } from './framework-library.service';
 export { NoFramework } from './no-framework/no.framework';
 export { NoFrameworkModule } from './no-framework/no-framework.module';
 
-export { MaterialDesignFramework } from './material-design-framework/material-design.framework';
-export { MaterialDesignFrameworkModule } from './material-design-framework/material-design-framework.module';
+// export { MaterialDesignFramework } from './material-design-framework/material-design.framework';
+// export { MaterialDesignFrameworkModule } from './material-design-framework/material-design-framework.module';
 
 export { Bootstrap3Framework } from './bootstrap-3-framework/bootstrap-3.framework';
 export { Bootstrap3FrameworkModule } from './bootstrap-3-framework/bootstrap-3-framework.module';
